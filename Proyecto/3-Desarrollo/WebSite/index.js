84cc2f2ffebae540f0a989383e6291f6f3d3302d
Otoniel hernnadez Garzon <hernandezottto@misena.edu.co>
2019-09-16 07:43:19 -0500

    Chore: se agrega conficguraciones de las rutas del directorio publico

--- Proyecto/3-Desarrollo/WebSite/index.js
+++ Proyecto/3-Desarrollo/WebSite/index.js
@@ -1,11 +1,12 @@
 'use strict'
- const express = require('express'),
+const express = require('express'),
+  path=require('path').join,
   app = express(),
+  publicDir=path(__dirname,'/public'),
   port = 3000;
   app.set('view engine', 'pug')
- 
-app.get('/', (req, res) => {
-  res.render('index')
-
-})
-   .listen(port, () => console.log(`Example app listening on port port!`))
+     .get('/', (req, res) => {
+      res.render('index')
+    })  
+  .use(express.static(publicDir))
+   .listen(port, () => console.log('http://localhost:'+port))
