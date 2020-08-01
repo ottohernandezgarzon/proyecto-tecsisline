173c57fac820fa422c2765132701e34527067d85
Otoniel Hernández Garzón <hernandezottto@misena.edu.co>
2020-08-01 11:52:09 -0500

    Chore: Se modifica la configuracion de la conexion

--- Proyecto/3-Desarrollo/WebSite/Models/database/dbConnection.js
+++ Proyecto/3-Desarrollo/WebSite/Models/database/dbConnection.js
@@ -1,35 +1,22 @@
-
 ' use strict ';
-// Recursos
-const 
-mysql = require('mysql'),
-conf = require('./configConnection.json');
-// Clase dbConnection 
-class dbConnection {
-  // atributo
-  op = {
-    host: conf.mysql.host,
-    port: conf.mysql.port,
-    user: conf.mysql.user,
-    password: conf.mysql.pass,
-    database: conf.mysql.db,
-  };
-  // método para la conexión
-  connect(con) {
-    let opCon = this.op;
-    con = mysql.createConnection(opCon);
-    con.connect(
-      (err) => {
-      return (err) ? 
-        console.error(`Error al Conectarse a MySQL: ${err.sqlMessage} \n verifica que esta encendido los servicios de base de datos`) 
-      :
-        console.log(`Conexión establecida con MySQL N°: ${con.threadId}
-        `);
-    });
-    return con;
 
-  }
+class dbConnection{
+	mysql(){
+		const db = require('mysql'),
+		 conf = require('./configConnection.json'),		
+		 dbOptions= {
+			 host : conf.mysql.host,
+			 port : conf.mysql.port,
+			 user : conf.mysql.user,
+			 password : conf.mysql.pass,
+			 database : conf.mysql.db
+		 },
+		 myConn = db.createConnection(dbOptions);
 
+		 myConn.connect((err) => {
+			 return (err) ? console.log(`Error al Conectarse a MySQL: ${err.stack}`) : console.log(`Conexión establecida con MySQL N°: ${myConn.threadId}`);
+		 });
+		 
+	}
 }
-// exporta en modulo de la clase dbConnection
 module.exports = dbConnection;
